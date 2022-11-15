e300f749bb8dfcb9cb5198660f3613d14d23f071
shiva shankar <sivas1475@gmail.com>
2022-11-16 04:06:13 +0530

    roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -31,3 +31,15 @@ ECHO "Install NodeJS Modules"
 cd /home/roboshop/catalogue && npm install &>>${LOG_FILE} && chown roboshop:roboshop /home/roboshop/catalogue -R
 statusCheck $?
 
+ECHO "Updated SystemD Configuration Files"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+statusCheck $?
+
+ECHO "Setup SystemD Service"
+mv /home/roboshop/catalogue/systemd.service  /etc/systemd/system/catalogue.service
+systemctl daemon-reload &>>${LOG_FILE} && systemctl enable catlogue &>>${LOG_FILE} && systemctl restart catalogue &>>${LOG_FILE}
+statusCheck $?
+
+echo 'Attempting to recover!' > /tmp/recovery_info
+systemctl reset-failed my-app
+systemctl restart my-app
