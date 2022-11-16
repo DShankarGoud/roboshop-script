e252410695ef2f62bad043122011b5e70bab9cf0
shiva shankar <sivas1475@gmail.com>
2022-11-17 02:05:42 +0530

    roboshop

--- components/frontend.sh
+++ components/frontend.sh
@@ -32,7 +32,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>${LOG_FILE}
 statusCheck $?
 
 ECHO "Update Nginx Configuration"
-sed -i -e '/catalogue/ s/localhiost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 statusCheck $?
 
 ECHO "Start Nginx Service"
