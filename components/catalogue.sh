33a1d6e97d91a67d5520da48d5c9ff4273fa1008
shiva shankar <sivas1475@gmail.com>
2022-11-16 19:13:54 +0530

    roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -7,3 +7,15 @@ ECHO "Configure NodeJS Yum Repos"
 curl -sL https://rpm.nodesource.com/setup_16.x | bash &>>${LOG_FILE}
 statusCheck $?
 
+ECHO "Install NodeJS"
+yum install nodejs -y gcc-c++ -y &>>${LOG_FILE}
+statusCheck $?
+
+
+id roboshop &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+  ECHO "Add Application User"
+  useradd roboshop &>>${LOG_FILE}
+  statusCheck $?
+fi
+
