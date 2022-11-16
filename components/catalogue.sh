20d0664b8268b50b4c3987ec4b6281290f9b9214
shiva shankar <sivas1475@gmail.com>
2022-11-16 19:15:57 +0530

    roboshop

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -27,3 +27,18 @@ ECHO "Extract Application Archive"
 cd /home/roboshop &&  rm -rf catalogue &>>${LOG_FILE} && unzip /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue
 statusCheck $?
 
+ECHO "Install NodeJS Modules"
+cd /home/roboshop/catalogue && npm install &>>${LOG_FILE} && chown roboshop:roboshop /home/roboshop/catalogue -R
+statusCheck $?
+
+ECHO "Updated SystemD Configuration Files"
+sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service
+statusCheck $?
+
+ECHO "Setup SystemD Service"
+mv /home/roboshop/catalogue/systemd.service  /etc/systemd/system/catalogue.service
+systemctl daemon-reload &>>${LOG_FILE} && systemctl enable catlogue &>>${LOG_FILE} && systemctl restart catalogue &>>${LOG_FILE}
+statusCheck $?
+
+
+
