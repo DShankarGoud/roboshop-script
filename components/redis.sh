a869d5880d5a83077fa6563614a68591d89b8af4
shiva shankar <sivas1475@gmail.com>
2022-11-17 02:21:22 +0530

    roboshop

---
+++ components/redis.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+source components/common.sh
+checkRootUser
+
+ECHO "Configure Yum Repos"
+curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Install Redis"
+yum install redis-6.2.7 -y &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Update Redis Configuration"
+sed -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Start Redios Service"
+systemctl enable redis &>>${LOG_FILE} && systemctl start redis &>>${LOG_FILE}
