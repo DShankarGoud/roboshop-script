94d615427fda87b96f74bf11a8c430af3cffce7b
shiva shankar <sivas1475@gmail.com>
2022-11-17 06:47:27 +0530

    roboshop

--- components/mysql.sh
+++ components/mysql.sh
@@ -26,7 +26,7 @@ if [ $? -ne 0 ]; then
   statusCheck $?
 fi
 
-echo 'show plugins' | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
+echo 'show plugins;' | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
   ECHO "Uninstall Password Validation Plugin"
   echo "uninstall plugin validate_password;" |  mysql -uroot -pRoboShop@1 &>>$LOG_FILE
