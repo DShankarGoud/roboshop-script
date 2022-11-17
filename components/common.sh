ed7a01c88fef6a805c9398126d207a324a8005ef
shiva shankar <sivas1475@gmail.com>
2022-11-18 00:44:31 +0530

    roboshop

--- components/common.sh
+++ components/common.sh
@@ -102,7 +102,7 @@ PYTHON() {
   statusCheck $?
 
   USER_ID=$(id -u roboshop)
-  GROUP_ID=$(ID -g roboshop)
+  GROUP_ID=$(id -g roboshop)
 
   ECHO "Update Roboshop Configuration"
   sed -i -e "/^uid/ c uid = ${USER_ID}" -e "/^gid/ c gid = ${GROUP_ID}" /home/roboshop/${COMPONENT}/${COMPONENT}.ini &>>${LOG_FILE}
